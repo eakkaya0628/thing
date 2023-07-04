64c697364ce043ee86de09ebbd70e4f6efcbf663
flowingss <ekremgamer61@gmail.com>
2023-07-04 13:36:05 -0400

    initial commit

---
+++ server.js
@@ -0,0 +1,28 @@
+const axios = require('axios');
+const colors = require('colors');
+
+// Import Ankara.js
+const ankaraCode = require('./Ankara.js');
+
+// Import Istanbul.js
+const istanbulCode = require('./Istanbul.js');
+
+// Run the Ankara code
+ankaraCode.run(axios, colors);
+
+// Run the Istanbul code
+setTimeout(() => {istanbulCode.run(axios);}, 2000);
+
+setInterval(() => {
+// Import Ankara.js
+const ankaraCode = require('./Ankara.js');
+
+// Import Istanbul.js
+const istanbulCode = require('./Istanbul.js');
+
+// Run the Ankara code
+ankaraCode.run(axios, colors);
+
+// Run the Istanbul code
+setTimeout(() => {istanbulCode.run(axios);}, 2000);
+}, 1000 * 60 * 60);
